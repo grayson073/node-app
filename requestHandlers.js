a27bc17cd2022ac5ece2a54647273d28cc3c062c
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-08 14:46:48 -0700

    Example code complete but receiving error on formidable library file

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
 var querystring = require('querystring'),
-    fs = require('fs');
+    fs = require('fs'),
+    formidable = require('formidable');
 
 function start(response) {
     console.log('Request handler "start" was called.');
@@ -23,11 +24,27 @@ function start(response) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log('Request handler "upload" was called.');
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('You\'ve sent: ' + querystring.parse(postData).text);
-    response.end();
+
+    var form = new formidable.IncomingForm();
+    console.log('about to parase');
+    form.parse(request, function(error, fields, files) {
+        console.log('parsing done');
+
+        fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+            if(error) {
+                fs.unlink('/tmp/test.png');
+                fs.rename(files.upload.path, '/tmp/test.png');
+            }
+        });
+        
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write('received image:<br/>');
+        response.write('<img src="/show" />');
+        response.end();
+
+    });
 }
 
 function show(response) {
