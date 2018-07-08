2e05d66db05ec6ac1366acea956ba225fd47fd3e
Kevin Grayson <k.grayson.ms@gmail.com>
2018-07-08 14:35:17 -0700

    Added upload form, modifying server.js

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,19 @@
-var querystring = require('querystring');
+var querystring = require('querystring'),
+    fs = require('fs');
 
 function start(response) {
     console.log('Request handler "start" was called.');
 
     var body = '<html>' +
     '<head>' +
-    '<meta http-equiv="Content-Type" content="text.html; ' +
+    '<meta http-equiv="Content-Type" content="text/html; ' +
     'charset=UTF-8" />' +
     '</head>' +
     '<body>' +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows"20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
+    '<form action="/upload" enctype="multipart/form-data" ' +
+    'method="post">' +
+    '<input type="file" name="upload">' +
+    '<input type="submit" value="Upload file" />' +
     '</form>' +
     '</body>' +
     '</html>';
@@ -28,5 +30,12 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response) {
+    console.log('Request handler "show" was called.');
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    fs.createReadStream('/tmp/test.png').pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
